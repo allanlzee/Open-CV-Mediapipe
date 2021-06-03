c9af08ebe0c792a36cc166db8b74c4ac16fccae0
Allan Zhou <allanlinzhou@gmail.com>
2021-06-03 19:06:51 -0400

    Added Progress Bar for Arm Trainer

--- Pose/PoseEstimationModule.py
+++ Pose/PoseEstimationModule.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import mediapipe as mp
 import time
+import math
 
-
-class poseDetector():
+class PoseDetector():
 
     def __init__(self, mode=False, upper_body=False, smooth=True,
                  detection_con=0.5, track_con=0.5):
@@ -31,7 +31,7 @@ class poseDetector():
         return frame
 
     def detect_position(self, frame, draw=True):
-        landmarks = []
+        self.landmarks = []
 
         if self.results.pose_landmarks:
             for id, landmark in enumerate(self.results.pose_landmarks.landmark):
@@ -40,12 +40,54 @@ class poseDetector():
 
                 pos_x, pos_y = int(landmark.x * width), int(landmark.y * height)
 
-                landmarks.append([id, pos_x, pos_y])
+                self.landmarks.append([id, pos_x, pos_y])
 
                 if draw:
                     cv.circle(frame, (pos_x, pos_y), 5, (255, 0, 0), -1)
 
-        return landmarks
+        return self.landmarks
+
+    def calculate_angle(self, frame, landmark_1, landmark_2, landmark_3, draw=True, color=(0, 255, 0)):
+
+        # Average the three landmarks to get the joint
+        self.joint = int((landmark_1 + landmark_2 + landmark_3) / 3)
+
+        length = len(self.landmarks)
+        if self.landmarks:
+            self.lm1_pos_x, self.lm1_pos_y = self.landmarks[landmark_1][1:]
+            self.lm2_pos_x, self.lm2_pos_y = self.landmarks[landmark_2][1:]
+            self.lm3_pos_x, self.lm3_pos_y = self.landmarks[landmark_3][1:]
+            self.joint_pos_x, self.joint_pos_y = self.landmarks[self.joint][1:]
+
+        if draw:
+            cv.line(frame, (self.lm1_pos_x, self.lm1_pos_y), (self.lm2_pos_x, self.lm2_pos_y), color, 3)
+            cv.line(frame, (self.lm2_pos_x, self.lm2_pos_y), (self.lm3_pos_x, self.lm3_pos_y), color, 3)
+            cv.line(frame, (self.lm1_pos_x, self.lm1_pos_y), (self.lm3_pos_x, self.lm3_pos_y), color, 3)
+
+            cv.circle(frame, (self.lm1_pos_x, self.lm1_pos_y), 10, (0, 0, 255), -1)
+            cv.circle(frame, (self.lm2_pos_x, self.lm2_pos_y), 10, (0, 0, 255), -1)
+            cv.circle(frame, (self.lm3_pos_x, self.lm3_pos_y), 10, (0, 0, 255), -1)
+            cv.circle(frame, (self.joint_pos_x, self.joint_pos_y), 10, (255, 0, 0), -1)
+
+        # Trigonometry to find angle of joints
+        self.landmark_joints = [13, 14, 25, 26]
+
+        # Left Arm
+        length1 = math.sqrt(
+            (self.lm1_pos_x - self.lm2_pos_x) * (self.lm1_pos_x - self.lm2_pos_x) + (self.lm1_pos_y - self.lm2_pos_y) * (self.lm1_pos_y - self.lm2_pos_y))
+        length2 = math.sqrt(
+            (self.lm1_pos_x - self.lm3_pos_x) * (self.lm1_pos_x - self.lm3_pos_x) + (self.lm1_pos_y - self.lm3_pos_y) * (self.lm1_pos_y - self.lm3_pos_y))
+        length3 = math.sqrt(
+            (self.lm2_pos_x - self.lm3_pos_x) * (self.lm2_pos_x - self.lm3_pos_x) + (self.lm2_pos_y - self.lm3_pos_y) * (self.lm2_pos_y - self.lm3_pos_y))
+        # print(length1, length2, length3)
+
+        # if int(length1) > 0 & int(length3) > 0:
+        angle = (length2 ** 2 - length1 ** 2 - length3 ** 2) / (-2 * length1 * length3)
+        joint_angle = math.degrees(math.acos(angle))
+        joint_angle = int(joint_angle)
+        print(joint_angle)
+
+        return frame, joint_angle
 
 
 def main():
@@ -53,7 +95,7 @@ def main():
     prev_time = 0
 
     # Use class
-    detector = poseDetector()
+    detector = PoseDetector()
 
     while True:
         is_true, frame = capture.read()
@@ -65,7 +107,7 @@ def main():
 
         if landmarks:
             print(landmarks[0])
-            cv.circle(frame, (landmarks[0][1], landmarks[0][2]), 10, (0, 0, 255), cv.FILLED)
+            # cv.circle(frame, (landmarks[0][1], landmarks[0][2]), 10, (0, 0, 255), cv.FILLED)
 
         # (landmarks[n][1], landmarks[n][2]) -> n is an int from 0 - 32 for various landmarks
         curr_time = time.time()
--- Pose/RightArmTrainer.py
+++ Pose/RightArmTrainer.py
@@ -8,20 +8,19 @@ prev_time = 0
 
 detector = PoseEstimationModule.PoseDetector(detection_con=0.8, track_con=0.8)
 
-rep_counter_left_arm = 0
 rep_counter_right_arm = 0
 
 # Boolean for the rep
-rep_left_arm = False
 rep_right_arm = False
+
+# Direction (0 -> UP, 1 -> DOWN)
+direction = 0
 
 while True:
     is_true, frame = capture.read()
     frame = detector.detect_pose(frame, draw=False)
 
     width, height, channels = frame.shape
-    width *= 0.75
-    height *= 0.75
 
     frame = cv.resize(frame, (int(height), int(width)))
 
@@ -29,29 +28,46 @@ while True:
     if landmarks:
         # print(landmarks)
         # Find the Angles of the Important Points
-        pass
+        # Arms
+        # frame, joint_angle_left_arm = detector.calculate_angle(frame, 11, 13, 15)
 
-    # Arms
-    # frame, joint_angle_left_arm = detector.calculate_angle(frame, 11, 13, 15)
+        frame, joint_angle_right_arm = detector.calculate_angle(frame, 12, 14, 16)
 
-    frame, joint_angle_right_arm = detector.calculate_angle(frame, 12, 14, 16)
+        # Legs
+        # frame = detector.calculate_angle(frame, 23, 25, 27, color=(255, 0, 255))
+        # frame = detector.calculate_angle(frame, 24, 26, 28, color=(255, 0, 255))
 
-    # Legs
-    # frame = detector.calculate_angle(frame, 23, 25, 27, color=(255, 0, 255))
-    # frame = detector.calculate_angle(frame, 24, 26, 28, color=(255, 0, 255))
+        # Calculate Repetitions
+        if int(joint_angle_right_arm) <= 35 and rep_right_arm is False:
+            rep_counter_right_arm += 1
+            rep_right_arm = True
+        elif int(joint_angle_right_arm) <= 35 and rep_right_arm is True:
+            rep_right_arm = True
+        else:
+            rep_right_arm = False
 
-    # Calculate Repetitions
-    if int(joint_angle_right_arm) <= 35 and rep_left_arm is False:
-        rep_counter_right_arm += 1
-        rep_left_arm = True
-    elif int(joint_angle_right_arm) <= 35 and rep_left_arm is True:
-        rep_left_arm = True
-    else:
-        rep_left_arm = False
+        percent = 100.0 - np.interp(joint_angle_right_arm, (35, 160), (0, 100))
+        progress_bar = int(-550 * percent * 0.01 + 650)
+        print(progress_bar)
 
-    # Display Reps
-    cv.putText(frame, f'Right Arm Reps: {str(rep_counter_right_arm)}', (10, 80), cv.FONT_HERSHEY_COMPLEX, 1,
-               (0, 255, 0), 2)
+        # Display Progress Bar
+        cv.rectangle(frame, (1100, 100), (1175, 650), (221, 163, 76), 2)
+
+        if percent <= 25.0:
+            cv.rectangle(frame, (1100, progress_bar), (1175, 650), (0, 0, 255), -1)
+        elif percent <= 50.0:
+            cv.rectangle(frame, (1100, progress_bar), (1175, 650), (0, 165, 255), -1)
+        elif percent <= 75.0:
+            cv.rectangle(frame, (1100, progress_bar), (1175, 650), (206, 241, 15), -1)
+        else:
+            cv.rectangle(frame, (1100, progress_bar), (1175, 650), (0, 255, 0), -1)
+
+        cv.putText(frame, f'{int(percent)}%', (1100, 75), cv.FONT_HERSHEY_COMPLEX, 2,
+                   (0, 255, 0), 2)
+
+        # Display Reps
+        cv.putText(frame, f'Left Arm Reps: {str(rep_counter_right_arm)}', (10, 80), cv.FONT_HERSHEY_COMPLEX, 1,
+                   (0, 255, 0), 2)
 
     # Frame Rates
     curr_time = time.time()
